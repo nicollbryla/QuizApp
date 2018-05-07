4ed0c6f7301ebd0368bd3a8355c9c0a630bce7f5
Klaudia Goska <klaudia.goska06@gmail.com>
2018-05-07 22:58:17 +0200

    Add .gitignore, cleanup

---
+++ create.sql
@@ -0,0 +1,28 @@
+CREATE TABLE admin
+(
+    id INTEGER PRIMARY KEY NOT NULL,
+    login CHAR(30) NOT NULL,
+    password CHAR(50) NOT NULL,
+    name CHAR(30),
+    surname CHAR(50)
+);
+CREATE TABLE player
+(
+    id INTEGER PRIMARY KEY NOT NULL,
+    login CHAR(30) NOT NULL,
+    password CHAR(50) NOT NULL,
+    name CHAR(30),
+    surname CHAR(50),
+    score INTEGER NOT NULL
+);
+CREATE TABLE questions (
+    id    INTEGER NOT NULL,
+    cat   TEXT NOT NULL,
+    content       TEXT NOT NULL,
+    ans0  TEXT NOT NULL,
+    ans1  TEXT NOT NULL,
+    ans2  TEXT NOT NULL,
+    ans3  TEXT NOT NULL,
+    PRIMARY KEY(id)
+);
+Insert into player(id, login, password, name, surname, score) values ('1', 'ResultSet', 'halko', 'ResultSet', 'ResultSet', '0');
