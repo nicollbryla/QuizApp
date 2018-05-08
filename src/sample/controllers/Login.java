nicollbryla/QuizApp

commit 72671d5dc37670661b6448337aefea110863aeb4

Basic questions display

src/sample/controllers/Login.java
@@ -16,7 +16,7 @@ import java.util.ResourceBundle;
 import sample.comp.Player;
 import sample.Main;
 
-public class Login implements Initializable {
+public class Login extends QuizController {
     @FXML
     public ToggleGroup logowanie;
     @FXML

src/sample/Main.java
@@ -10,6 +10,7 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import sample.controllers.QuizController;
 
 import java.io.IOException;
 
@@ -38,20 +39,26 @@ public class Main extends Application {
     //jak zrobie css to wprowadze jeszcze jedna zmienna do tej metody. narazie niech sie tak nazywa
     public static void zmiana_strony_css(ActionEvent actionEvent, Player player, FXMLLoader loader, String fxmlfile, Admin admin) throws IOException {
         Parent homePageParent = loader.load();
+        QuizController controller = loader.getController();
+        controller.setPlayer(player);
         Scene homePageScene = new Scene(homePageParent);
+        Stage appStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
         if (fxmlfile.equals("mainWindow")) {
             homePageParent.setId("pane");
         }
         else if (fxmlfile.equals("rejestracja")){
             homePageParent.setId("rejestracja");
         }
-        else if (fxmlfile.equals("logowanie")) {
+        else if (fxmlfile.equals("logowanie")){
             homePageParent.setId("pane");
         }
-        else if (fxmlfile.equals("guest")) {
+        else if (fxmlfile.equals("onePlayer")){
+            homePageParent.setId("onePlayer");
+            appStage.hide();
+            appStage = new Stage();
+        } else if (fxmlfile.equals("guest")) {
             homePageParent.setId("pane");
         }
-        Stage appStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
         appStage.setScene(homePageScene);
         appStage.show();
     }

src/sample/comp/Player.java
@@ -22,5 +22,6 @@ public class Player {
         name = rs.getString("name");
         surname = rs.getString("surname");
         login = rs.getString("login");
+        score = 0;
     }
 }

src/sample/controllers/MainWindow.java
@@ -1,21 +1,25 @@
 package sample.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import sample.Main;
 import sample.comp.Player;
 import javafx.fxml.Initializable;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class MainWindow implements Initializable {
-    private Player player;
-
-    public void addClient(Player player){
-        this.player = player;
-        System.out.println("Przekazano " + player.name);
-    }
+public class MainWindow extends QuizController {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
     }
+
+    public void onePlayer(ActionEvent actionEvent) throws IOException{
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("../fxml/OnePlayer.fxml"));
+        Main.zmiana_strony_css(actionEvent, player, loader, "onePlayer", null);
+    }
+
 }

src/sample/controllers/OnePlayer.java
@@ -1,0 +1,84 @@
+package sample.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
+import sample.Main;
+import sample.comp.Player;
+import sample.comp.Question;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.*;
+
+public class OnePlayer extends QuizController {
+    @FXML
+    private Label questionLabel;
+    @FXML
+    private Label categoryLabel;
+    @FXML
+    private RadioButton answer0;
+    @FXML
+    private RadioButton answer1;
+    @FXML
+    private RadioButton answer2;
+    @FXML
+    private RadioButton answer3;
+    @FXML
+    private Label pointsLabel;
+
+    public void setPlayer(Player p){
+        player = p;
+        displayQuestion();
+    }
+
+    private Random random;
+    private List<Question> questionList;
+    private int questionIndex;
+    private Question currentQuestion;
+
+    private void displayQuestion(){
+        questionLabel.setText(currentQuestion.content);
+        categoryLabel.setText("Kategoria: " + currentQuestion.category);
+        pointsLabel.setText("Twoje punkty: " + Integer.toString(player.score));
+        answer0.setText(currentQuestion.ans[0]);
+        answer1.setText(currentQuestion.ans[1]);
+        answer2.setText(currentQuestion.ans[2]);
+        answer3.setText(currentQuestion.ans[3]);
+    }
+
+    private boolean nextQuestion(){
+        if(questionIndex + 1 == questionList.size())
+            return false;
+        currentQuestion = questionList.get(++questionIndex);
+        currentQuestion.shuffle(random);
+        return true;
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        ToggleGroup group = new ToggleGroup();
+        answer0.setToggleGroup(group);
+        answer1.setToggleGroup(group);
+        answer2.setToggleGroup(group);
+        answer3.setToggleGroup(group);
+        random = new Random();
+        questionList = Question.loadQuestions();
+        questionIndex = -1;
+        Collections.shuffle(questionList, random);
+        nextQuestion();
+    }
+
+    public void answer(ActionEvent actionEvent) throws IOException {
+        answer0.setSelected(false);
+        answer1.setSelected(false);
+        answer2.setSelected(false);
+        answer3.setSelected(false);
+        if(nextQuestion())
+            displayQuestion();
+    }
+}

src/sample/controllers/QuizController.java
@@ -1,0 +1,15 @@
+package sample.controllers;
+
+import javafx.fxml.Initializable;
+import sample.comp.Player;
+
+public abstract class QuizController implements Initializable {
+    Player player;
+
+    public void setPlayer(Player player){
+        this.player = player;
+    }
+    public Player getPlayer(){
+        return this.player;
+    }
+}

src/sample/controllers/Registration.java
@@ -14,7 +14,7 @@ import java.net.URL;
 import java.sql.ResultSet;
 import java.util.ResourceBundle;
 
-public class Registration implements Initializable{
+public class Registration extends QuizController {
 
     public PasswordField pass1;
     public PasswordField pass2;

src/sample/fxml/OnePlayer.fxml
@@ -1,0 +1,44 @@
+<?xml version="1.0" encoding="UTF-8"?>
+
+<?import javafx.scene.control.Button?>
+<?import javafx.scene.control.Label?>
+<?import javafx.scene.control.RadioButton?>
+<?import javafx.scene.layout.AnchorPane?>
+<?import javafx.scene.layout.ColumnConstraints?>
+<?import javafx.scene.layout.GridPane?>
+<?import javafx.scene.layout.RowConstraints?>
+<?import javafx.scene.text.Font?>
+
+<AnchorPane maxHeight="-Infinity" maxWidth="-Infinity" minHeight="600.0" minWidth="900.0" prefHeight="600.0" prefWidth="900.0" xmlns="http://javafx.com/javafx/8.0.141" xmlns:fx="http://javafx.com/fxml/1" fx:controller="sample.controllers.OnePlayer">
+   <children>
+      <AnchorPane layoutX="14.0" layoutY="14.0" prefHeight="151.0" prefWidth="796.0" style="-fx-border-style: solid; -fx-border-width: 2px;" AnchorPane.leftAnchor="14.0" AnchorPane.rightAnchor="14.0" AnchorPane.topAnchor="100.0">
+         <children>
+            <Label fx:id="questionLabel" alignment="CENTER" prefHeight="152.0" prefWidth="572.0" text="Label" AnchorPane.bottomAnchor="2.0" AnchorPane.leftAnchor="-2.0" AnchorPane.rightAnchor="2.0" AnchorPane.topAnchor="-2.0" />
+            <Label fx:id="categoryLabel" layoutX="14.0" layoutY="4.0" text="Label" AnchorPane.rightAnchor="10.0" AnchorPane.topAnchor="10.0" />
+         </children>
+      </AnchorPane>
+      <Label alignment="CENTER" layoutX="14.0" layoutY="14.0" prefHeight="68.0" prefWidth="793.0" text="Odpowiedz na pytanie:" AnchorPane.leftAnchor="14.0" AnchorPane.rightAnchor="14.0" AnchorPane.topAnchor="14.0">
+         <font>
+            <Font size="25.0" />
+         </font>
+      </Label>
+      <Label fx:id="pointsLabel" layoutX="14.0" layoutY="264.0" text="Label" AnchorPane.leftAnchor="14.0" />
+      <Button layoutX="362.0" layoutY="544.0" mnemonicParsing="false" onAction="#answer" prefHeight="43.0" prefWidth="211.0" text="Odpowiadam" AnchorPane.bottomAnchor="36.0" AnchorPane.leftAnchor="362.0" AnchorPane.rightAnchor="362.0" />
+      <GridPane layoutX="35.0" layoutY="320.0" prefHeight="167.0" prefWidth="792.0" AnchorPane.leftAnchor="50.0" AnchorPane.rightAnchor="50.0">
+        <columnConstraints>
+          <ColumnConstraints halignment="CENTER" hgrow="SOMETIMES" minWidth="10.0" prefWidth="100.0" />
+          <ColumnConstraints halignment="CENTER" hgrow="SOMETIMES" minWidth="10.0" prefWidth="100.0" />
+        </columnConstraints>
+        <rowConstraints>
+          <RowConstraints minHeight="10.0" prefHeight="30.0" vgrow="SOMETIMES" />
+          <RowConstraints minHeight="10.0" prefHeight="30.0" vgrow="SOMETIMES" />
+        </rowConstraints>
+         <children>
+            <RadioButton fx:id="answer0" contentDisplay="CENTER" mnemonicParsing="false" prefHeight="68.0" prefWidth="306.0" text="RadioButton" textAlignment="CENTER" />
+            <RadioButton fx:id="answer2" mnemonicParsing="false" prefHeight="68.0" prefWidth="306.0" text="RadioButton" GridPane.rowIndex="1" />
+            <RadioButton fx:id="answer1" mnemonicParsing="false" prefHeight="68.0" prefWidth="306.0" text="RadioButton" GridPane.columnIndex="1" />
+            <RadioButton fx:id="answer3" mnemonicParsing="false" prefHeight="68.0" prefWidth="306.0" text="RadioButton" GridPane.columnIndex="1" GridPane.rowIndex="1" />
+         </children>
+      </GridPane>
+   </children>
+</AnchorPane>

commit 191a365a2571b6e2c628d86004353aa28c4c5732

Minor bugfix
Fixes bug introduced in 72671d5d.

src/sample/controllers/Login.java
@@ -133,7 +133,7 @@ public class Login extends QuizController {
 
     public void asGuest(ActionEvent actionEvent) throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("../fxml/mainWindow.fxml"));
-        Main.zmiana_strony_css(actionEvent, null, loader, "guest", null);
+        Main.zmiana_strony_css(actionEvent, new Player(), loader, "guest", null);
     }
 
     public void signUp(ActionEvent actionEvent) throws IOException {